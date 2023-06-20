3ee57afd3213d5501b9b074e2031d1bcd3f1094f
Kae <80987908+Novaenia@users.noreply.github.com>
2023-06-20 17:20:08 +1000

    tweak win64 setup script

--- scripts/windows/setup64.bat
+++ scripts/windows/setup64.bat
@@ -9,6 +9,8 @@ copy scripts\windows\sbinit.config dist\
 mkdir build
 cd build
 
+del /f CMakeCache.txt
+
 if exist "C:\Program Files (x86)\CMake\bin" (
   set CMAKE_EXE_PATH="C:\Program Files (x86)\CMake\bin"
 ) else (
@@ -20,22 +22,28 @@ set QT_PREFIX_PATH=C:\Qt\5.6\msvc2015_64
 if exist %QT_PREFIX_PATH% (
 
 %CMAKE_EXE_PATH%\cmake.exe ^
-  ..\source ^
-  -G"Visual Studio 15 Win64" ^
-  -DSTAR_USE_JEMALLOC=ON ^
+  -G"Visual Studio 17 2022" ^
+  -T"v143" ^
   -DCMAKE_PREFIX_PATH=%QT_PREFIX_PATH% ^
+  -DSTAR_USE_JEMALLOC=OFF ^
+  -DSTAR_ENABLE_STEAM_INTEGRATION=ON ^
+  -DSTAR_ENABLE_DISCORD_INTEGRATION=ON ^
   -DSTAR_BUILD_QT_TOOLS=ON ^
   -DCMAKE_INCLUDE_PATH="..\lib\windows64\include" ^
-  -DCMAKE_LIBRARY_PATH="..\lib\windows64"
+  -DCMAKE_LIBRARY_PATH="..\lib\windows64" ^
+  ..\source
 
 ) else (
 
 %CMAKE_EXE_PATH%\cmake.exe ^
-  ..\source ^
-  -G "Visual Studio 15 Win64" ^
-  -DSTAR_USE_JEMALLOC=ON ^
+  -G"Visual Studio 17 2022" ^
+  -T"v143" ^
+  -DSTAR_USE_JEMALLOC=OFF ^
+  -DSTAR_ENABLE_STEAM_INTEGRATION=ON ^
+  -DSTAR_ENABLE_DISCORD_INTEGRATION=ON ^
   -DCMAKE_INCLUDE_PATH="..\lib\windows64\include" ^
-  -DCMAKE_LIBRARY_PATH="..\lib\windows64"
+  -DCMAKE_LIBRARY_PATH="..\lib\windows64" ^
+  ..\source
 
 )
 
