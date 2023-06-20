0951cfab48fdc5334bc949ff25d856e57c43b9e9
Kae <80987908+Novaenia@users.noreply.github.com>
2023-06-20 14:35:39 +1000

    tweak win64 build script

--- scripts/gitlab-ci/windows64/build.bat
+++ scripts/gitlab-ci/windows64/build.bat
@@ -8,13 +8,13 @@ cd build || exit /b 1
 del /f CMakeCache.txt
 
 cmake.exe ^
-  -G"Visual Studio 14 Win64" ^
-  -T"v140" ^
+  -G"Visual Studio 17 2022" ^
+  -T"v143" ^
   -DCMAKE_PREFIX_PATH=%QT_PREFIX_PATH% ^
   -DSTAR_USE_JEMALLOC=OFF ^
   -DSTAR_ENABLE_STEAM_INTEGRATION=ON ^
   -DSTAR_ENABLE_DISCORD_INTEGRATION=ON ^
-  -DSTAR_BUILD_QT_TOOLS=ON ^
+  -DSTAR_BUILD_QT_TOOLS=OFF ^
   -DCMAKE_INCLUDE_PATH="..\lib\windows64\include" ^
   -DCMAKE_LIBRARY_PATH="..\lib\windows64" ^
   ..\source || exit /b 1
@@ -25,6 +25,8 @@ cd ..
 
 move dist windows64_binaries || exit /b 1
 
-windeployqt.exe windows64_binaries\mod_uploader.exe || exit /b 1
+if exist windows64_binaries\mod_uploader.exe (
+  windeployqt.exe windows64_binaries\mod_uploader.exe || exit /b 1
+)
 
 copy lib\windows64\*.dll windows64_binaries\ || exit /b 1
