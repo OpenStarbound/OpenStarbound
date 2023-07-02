d7ba1136883de9392bb929df5772dba7f8bf49df
Kae <80987908+Novaenia@users.noreply.github.com>
2023-07-03 04:29:58 +1000

    Add zoom level keybinds

--- source/client/StarClientApplication.cpp
+++ source/client/StarClientApplication.cpp
@@ -315,6 +315,18 @@ void ClientApplication::processInput(InputEvent const& event) {
   }
 
   m_input->handleInput(event, processed);
+  WorldCamera& camera = m_worldPainter->camera();
+
+  auto config = m_root->configuration();
+  int zoomOffset = 0;
+
+  if (auto presses = m_input->bindDown("opensb", "zoomIn"))
+    zoomOffset += *presses;
+  if (auto presses = m_input->bindDown("opensb", "zoomOut"))
+    zoomOffset -= *presses;
+
+  if (zoomOffset != 0)
+    config->set("zoomLevel", max(1.0f, round(config->get("zoomLevel").toFloat() + zoomOffset)));
 }
 
 void ClientApplication::update() {
