OpenStarbound/OpenStarbound

commit cd23817bf1884fee4457ab3381320b0c8fc33f48

Fixed remaining compiler warnings
Some warnings (e.g. in Opus and the tests) can't be fixed, only suppressed.

source/frontend/StarMainInterface.cpp
@@ -1365,11 +1365,9 @@ void MainInterface::renderDebug() {
 
     m_debugTextRect = RectF::null();
 
-    counter = 0;
-    for (auto const& pair : logMapValues) {
+    for (size_t index = 0; index < logMapValues.size(); ++index) {
       TextPositioning positioning = { Vec2F(m_config->debugOffset[0], windowHeight() - m_config->debugOffset[1] - m_config->fontSize * interfaceScale() * counter) };
-      m_guiContext->renderText(formatted[counter], positioning);
-      ++counter;
+      m_guiContext->renderText(formatted[index], positioning);
     }
     m_guiContext->setFontSize(8);
     m_guiContext->setDefaultFont();
@@ -1515,7 +1513,7 @@ bool MainInterface::overButton(PolyI buttonPoly, Vec2I const& mousePos) const {
   return buttonPoly.contains(mousePos);
 }
 
-bool MainInterface::overlayClick(Vec2I const& mousePos, MouseButton mouseButton) {
+bool MainInterface::overlayClick(Vec2I const& mousePos, MouseButton) {
   PolyI mainBarPoly = m_config->mainBarPoly;
   Vec2I barPos = mainBarPosition();
   mainBarPoly.translate(barPos);

source/application/StarMainApplication_sdl.cpp
@@ -819,7 +819,7 @@ private:
 
       auto newImage = std::make_shared<Image>(processImageOperations(operations, *image));
       // Fix fully transparent pixels inverting the underlying display pixel on Windows (allowing this could be made configurable per cursor later!)
-      newImage->forEachPixel([](unsigned x, unsigned y, Vec4B& pixel) { if (!pixel[3]) pixel[0] = pixel[1] = pixel[2] = 0; });
+      newImage->forEachPixel([](unsigned /*x*/, unsigned /*y*/, Vec4B& pixel) { if (!pixel[3]) pixel[0] = pixel[1] = pixel[2] = 0; });
       entry->image = std::move(newImage);
       
 

source/application/StarP2PNetworkingService_pc.cpp
@@ -81,7 +81,7 @@ void PcP2PNetworkingService::setJoinRemote(HostAddressWithPort location) {
   setJoinLocation(JoinRemote(location));
 }
 
-void Star::PcP2PNetworkingService::setActivityData(String const& title, Maybe<pair<uint16_t, uint16_t>> party) {
+void Star::PcP2PNetworkingService::setActivityData(String const&, Maybe<pair<uint16_t, uint16_t>>) {
 #ifdef STAR_ENABLE_DISCORD_INTEGRATION
   MutexLocker discordLocker(m_state->discordMutex);
 #endif

source/application/StarPlatformServices_pc.cpp
@@ -98,7 +98,7 @@ void PcPlatformServicesState::onGameOverlayActivated(GameOverlayActivated_t* cal
 }
 #endif
 
-PcPlatformServicesUPtr PcPlatformServices::create(String const& path, StringList platformArguments) {
+PcPlatformServicesUPtr PcPlatformServices::create(String const&, StringList platformArguments) {
   auto services = unique_ptr<PcPlatformServices>(new PcPlatformServices);
 
   services->m_state = make_shared<PcPlatformServicesState>();

source/application/StarRenderer_opengl20.cpp
@@ -720,7 +720,6 @@ void OpenGl20Renderer::GlRenderBuffer::set(List<RenderPrimitive>& primitives) {
 
   float textureIndex = 0.0f;
   Vec2F textureOffset = {};
-  Texture* lastTexture = nullptr;
   for (auto& primitive : primitives) {
     if (auto tri = primitive.ptr<RenderTriangle>()) {
       tie(textureIndex, textureOffset) = addCurrentTexture(std::move(tri->texture));
@@ -781,7 +780,7 @@ bool OpenGl20Renderer::logGlErrorSummary(String prefix) {
       } else {
         Logger::error("<UNRECOGNIZED GL ERROR>");
       }
-    } while (error = glGetError());
+    } while ((error = glGetError()));
     return true;
   }
   return false;
@@ -897,8 +896,6 @@ void OpenGl20Renderer::renderGlBuffer(GlRenderBuffer const& renderBuffer, Mat3F 
 
 //Assumes the passed effect program is currently in use.
 void OpenGl20Renderer::setupGlUniforms(Effect& effect) {
-  GLuint program = effect.program;
-
   m_positionAttribute = effect.getAttribute("vertexPosition");
   m_texCoordAttribute = effect.getAttribute("vertexTextureCoordinate");
   m_texIndexAttribute = effect.getAttribute("vertexTextureIndex");

source/base/StarAssets.cpp
@@ -858,7 +858,7 @@ shared_ptr<Assets::AssetData> Assets::loadImage(AssetPath const& path) const {
     for (auto& directives : path.directives.list())
       directives.loadOperations();
 
-    path.directives.forEach([&](auto const& entry, Directives const& directives) {
+    path.directives.forEach([&](auto const& entry, Directives const&) {
       addImageOperationReferences(entry.operation, referencePaths);
     }); // TODO: This can definitely be better, was changed quickly to support the new Directives.
 
@@ -875,7 +875,7 @@ shared_ptr<Assets::AssetData> Assets::loadImage(AssetPath const& path) const {
     return unlockDuring([&]() {
       auto newData = make_shared<ImageData>();
       Image newImage = *source->image;
-      path.directives.forEach([&](auto const& entry, Directives const& directives) {
+      path.directives.forEach([&](auto const& entry, Directives const&) {
         if (auto error = entry.operation.template ptr<ErrorImageOperation>())
           std::rethrow_exception(error->exception);
         else

source/base/StarMixer.cpp
@@ -473,7 +473,7 @@ void Mixer::setGroupVolume(MixerGroup group, float targetValue, float rampTime) 
   }
 }
 
-void Mixer::update(float dt, PositionalAttenuationFunction positionalAttenuationFunction) {
+void Mixer::update(float, PositionalAttenuationFunction positionalAttenuationFunction) {
   {
     MutexLocker locker(m_queueMutex);
     eraseWhere(m_audios, [&](auto& p) {

source/client/StarClientApplication.cpp
@@ -333,7 +333,6 @@ void ClientApplication::processInput(InputEvent const& event) {
   }
 
   m_input->handleInput(event, processed);
-  WorldCamera& camera = m_worldPainter->camera();
 
   auto config = m_root->configuration();
   int zoomOffset = 0;
@@ -709,7 +708,7 @@ void ClientApplication::updateMods(float dt) {
   }
 }
 
-void ClientApplication::updateModsWarning(float dt) {
+void ClientApplication::updateModsWarning(float) {
   if (m_errorScreen->accepted())
     changeState(MainAppState::Splash);
 }
@@ -720,7 +719,7 @@ void ClientApplication::updateSplash(float dt) {
     changeState(MainAppState::Title);
 }
 
-void ClientApplication::updateError(float dt) {
+void ClientApplication::updateError(float) {
   if (m_errorScreen->accepted())
     changeState(MainAppState::Title);
 }

commit 3453819837f8f1bb1d439bb2df66386a9f35d80d

fix debug logMap text stacking
bug was introduced in cd23817bf1884fee4457ab3381320b0c8fc33f48

source/frontend/StarMainInterface.cpp
@@ -1365,10 +1365,11 @@ void MainInterface::renderDebug() {
 
     m_debugTextRect = RectF::null();
 
-    for (size_t index = 0; index < logMapValues.size(); ++index) {
-      TextPositioning positioning = { Vec2F(m_config->debugOffset[0], windowHeight() - m_config->debugOffset[1] - m_config->fontSize * interfaceScale() * counter) };
+    for (size_t index = 0; index != formatted.size(); ++index) {
+      TextPositioning positioning = { Vec2F(m_config->debugOffset[0], windowHeight() - m_config->debugOffset[1] - m_config->fontSize * interfaceScale() * index) };
       m_guiContext->renderText(formatted[index], positioning);
     }
+
     m_guiContext->setFontSize(8);
     m_guiContext->setDefaultFont();
     m_guiContext->setDefaultLineSpacing();