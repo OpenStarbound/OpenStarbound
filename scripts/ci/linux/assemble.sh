a5f42ce40a848e3ef66dd6b0bda0fd94954fb6bb
Kae <80987908+Novaenia@users.noreply.github.com>
2024-07-30 12:13:43 +1000

    improve linux server compatibility

--- scripts/ci/linux/assemble.sh
+++ scripts/ci/linux/assemble.sh
@@ -34,6 +34,15 @@ touch server_distribution/mods/mods_go_here
 
 mkdir server_distribution/linux
 
+# makes the server function on older Linux versions (this is so stupid)
+nm --dynamic --undefined-only --with-symbol-versions dist/starbound_server | grep GLIBC_2.29
+./scripts/ci/linux/patchelf dist/starbound_server \
+  --clear-symbol-version exp \
+  --clear-symbol-version exp2 \
+  --clear-symbol-version log \
+  --clear-symbol-version log2 \
+  --clear-symbol-version pow
+
 cp \
   dist/starbound_server \
   dist/btree_repacker \
