0c4682d304d37e0c634ddf90dc49a0560310c9ab
Bottinator22 <59987380+Bottinator22@users.noreply.github.com>
2024-12-18 22:26:59 -0800

    Update StarGraphicsMenu.hpp

--- source/frontend/StarGraphicsMenu.hpp
+++ source/frontend/StarGraphicsMenu.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
 #include "StarPane.hpp"
+#include "StarMainInterfaceTypes.hpp"
+#include "StarUniverseClient.hpp"
 
 namespace Star {
 
 STAR_CLASS(GraphicsMenu);
+STAR_CLASS(ShadersMenu);
 
 class GraphicsMenu : public Pane {
 public:
-  GraphicsMenu();
+  GraphicsMenu(PaneManager* manager,UniverseClientPtr client);
 
   void show() override;
   void dismissed() override;
@@ -23,6 +26,8 @@ private:
 
   void apply();
   void applyWindowSettings();
+  
+  void displayShaders();
 
   List<Vec2U> m_resList;
   List<int> m_interfaceScaleList;
@@ -30,6 +35,9 @@ private:
   List<float> m_cameraSpeedList;
 
   JsonObject m_localChanges;
+  
+  ShadersMenuPtr m_shadersMenu;
+  PaneManager* m_paneManager;
 };
 
 }
