202de54f33ca4dd061ec612100bdd41b263fb71e
Kae <80987908+Novaenia@users.noreply.github.com>
2025-01-12 19:12:04 +1100

    Revert "shipworlds: store their own epoch time for consistent plant growth"
    This reverts commit 25f3edbae301eae0fd87162527e12cbb2630b022.

--- source/game/StarUniverseServer.cpp
+++ source/game/StarUniverseServer.cpp
@@ -682,6 +682,7 @@ void UniverseServer::updateShips() {
         Json jOldShipLevel = shipWorld->getProperty("ship.level");
         unsigned newShipLevel = min<unsigned>(speciesShips.size() - 1, newShipUpgrades.shipLevel);
 
+
         if (jOldShipLevel.isType(Json::Type::Int)) {
           auto oldShipLevel = jOldShipLevel.toUInt();
           if (oldShipLevel < newShipLevel) {
@@ -2047,21 +2048,10 @@ Maybe<WorkerPoolPromise<WorldServerThreadPtr>> UniverseServer::shipWorldPromise(
         shipWorld->setProperty("ship.maxFuel", currentUpgrades.maxFuel);
         shipWorld->setProperty("ship.crewSize", currentUpgrades.crewSize);
         shipWorld->setProperty("ship.fuelEfficiency", currentUpgrades.fuelEfficiency);
-        shipWorld->setProperty("ship.epoch", Time::timeSinceEpoch());
-      }
-
-      auto shipClock = make_shared<Clock>();
-      auto shipTime = shipWorld->getProperty("ship.epoch");
-      if (!shipTime.canConvert(Json::Type::Float)) {
-        auto now = Time::timeSinceEpoch();
-        shipWorld->setProperty("ship.epoch", now);
-      } else {
-        shipClock->setTime(Time::timeSinceEpoch() - shipTime.toDouble());
       }
 
       shipWorld->setUniverseSettings(m_universeSettings);
-      shipWorld->setReferenceClock(shipClock);
-      shipClock->start();
+      shipWorld->setReferenceClock(universeClock);
 
       if (auto systemWorld = clientContext->systemWorld())
         shipWorld->setOrbitalSky(systemWorld->clientSkyParameters(clientContext->clientId()));
