5dda9f47306a6eea5eb4e676c68f81b8285d9dfe
Kae <80987908+Novaenia@users.noreply.github.com>
2025-02-23 09:03:38 +1100

    add dismiss on swap checkbox

--- source/frontend/StarCharSelection.cpp
+++ source/frontend/StarCharSelection.cpp
@@ -39,6 +39,10 @@ CharSelectionPane::CharSelectionPane(PlayerStoragePtr playerStorage,
   guiReader.registerCallback("clearSearch", [=](Widget*) {
     fetchChild<TextBoxWidget>("searchCharacter")->setText("");
   });
+  guiReader.registerCallback("toggleDismissCheckbox", [=](Widget* widget) {
+    auto configuration = Root::singleton().configuration();
+    configuration->set("characterSwapDismisses", as<ButtonWidget>(widget)->isChecked());
+  });
 
   guiReader.construct(root.assets()->json("/interface/windowconfig/charselection.config"), this);
 }
