02566b69572d0c2f65b44744d732e8316e2c1bd9
grbr404 <166065505+grbr404@users.noreply.github.com>
2025-05-02 22:54:14 +0200

    Use Zink for OpenGL on Linux if supported
    If your system supports the Zink driver, you can use it for OpenGL rendering by switching to the Vulkan backend. This can improve performance.

--- scripts/ci/linux/run-client.sh
+++ scripts/ci/linux/run-client.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Check for Zink support
+if MESA_LOADER_DRIVER_OVERRIDE=zink glxinfo | grep -q 'renderer string.*zink'; then
+    # Set environment variables for Zink
+    export __GLX_VENDOR_LIBRARY_NAME=mesa
+    export MESA_LOADER_DRIVER_OVERRIDE=zink
+    export GALLIUM_DRIVER=zink
+fi
+
 cd "`dirname \"$0\"`"
 
 LD_LIBRARY_PATH="$LD_LIBRARY_PATH:./" ./starbound "$@"
