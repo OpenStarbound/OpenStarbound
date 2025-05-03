3fc211bcb8dcddd90d50b081553750573bddea7d
grbr404 <166065505+grbr404@users.noreply.github.com>
2025-05-03 19:23:13 +0200

    Update run-client.sh
    Added Vulkan support check befor looking for Zink support

--- scripts/ci/linux/run-client.sh
+++ scripts/ci/linux/run-client.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-# Check for Zink support
-if MESA_LOADER_DRIVER_OVERRIDE=zink glxinfo | grep -q 'renderer string.*zink'; then
-    # Set environment variables for Zink
-    export __GLX_VENDOR_LIBRARY_NAME=mesa
-    export MESA_LOADER_DRIVER_OVERRIDE=zink
-    export GALLIUM_DRIVER=zink
+# Check for Vulkan support
+if command -v vulkaninfo &> /dev/null; then
+    # Check for Zink support
+    if MESA_LOADER_DRIVER_OVERRIDE=zink glxinfo | grep -q 'renderer string.*zink'; then
+        # Set environment variables for Zink
+        export __GLX_VENDOR_LIBRARY_NAME=mesa
+        export MESA_LOADER_DRIVER_OVERRIDE=zink
+        export GALLIUM_DRIVER=zink
+    fi
 fi
 
 cd "`dirname \"$0\"`"
