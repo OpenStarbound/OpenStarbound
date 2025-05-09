cfd7f24803b6de2e007d506cf62ce8740fbfe4e9
grbr404 <166065505+grbr404@users.noreply.github.com>
2025-05-09 19:48:18 +0200

    Update run-client.sh
    Check if user already has set any of the variables to avoid overwriting them

--- scripts/ci/linux/run-client.sh
+++ scripts/ci/linux/run-client.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-# Check for Vulkan support
-if command -v vulkaninfo &> /dev/null; then
-    # Check for Zink support
-    if __GLX_VENDOR_LIBRARY_NAME=mesa MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink glxinfo | grep -q 'renderer string: zink'; then
-        # Set environment variables for Zink
-        export __GLX_VENDOR_LIBRARY_NAME=mesa MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink
+# Check if environment variables are already used by user
+if [ -n "${__GLX_VENDOR_LIBRARY_NAME}" ] || [ -n "${MESA_LOADER_DRIVER_OVERRIDE}" ] || [ -n "${GALLIUM_DRIVER}" ]; then :
+else
+    # Check for Vulkan support
+    if command -v vulkaninfo &> /dev/null; then
+        # Check for Zink support
+        if __GLX_VENDOR_LIBRARY_NAME=mesa MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink glxinfo | grep -q 'renderer string: zink'; then
+            # Set environment variables for Zink
+            export __GLX_VENDOR_LIBRARY_NAME=mesa MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink
+        fi
     fi
 fi
 
