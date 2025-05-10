ab389d7a2c61e73833ad81e3b50bedc9951d1fbf
WasabiRaptor <ketchupraptor@gmail.com>
2025-05-10 12:20:14 -0400

    missed entityResource

--- source/game/scripting/StarWorldLuaBindings.cpp
+++ source/game/scripting/StarWorldLuaBindings.cpp
@@ -605,6 +605,11 @@ namespace LuaBindings {
           return entity->statusController()->resourceNames();
         return {};
       });
+    callbacks.registerCallback("entityResource", [world](EntityId entityId, String name) -> Maybe<float> {
+        if (auto entity = world->get<ActorEntity>(entityId))
+          return entity->statusController()->resource(name);
+        return {};
+      });
     callbacks.registerCallback("entityIsResource", [world](EntityId entityId, String name) -> Maybe<bool> {
         if (auto entity = world->get<ActorEntity>(entityId))
           return entity->statusController()->isResource(name);
