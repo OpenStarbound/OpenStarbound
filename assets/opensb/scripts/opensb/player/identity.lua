a750de7abe515e186a30f8b31d746ed8e61a2747
WasabiRaptor <ketchupraptor@gmail.com>
2025-08-14 22:48:19 -0400

    update /identity command with randomize and save/load

--- assets/opensb/scripts/opensb/player/identity.lua
+++ assets/opensb/scripts/opensb/player/identity.lua
@@ -3,35 +3,61 @@ modules.identity = module
 local commands = modules.commands
 
 commands.register("identity", function(args)
-  local method, path, value = chat.parseArguments(args)
+  local method, arg1, arg2 = chat.parseArguments(args)
   if not method or method == "get" or method == "" then
     local identity = player.humanoidIdentity()
-    if path then
-      value = identity[path]
-      if value or path == "imagePath" then
-        return type(value) == "string" and value or sb.printJson(value, 1)
+    if arg1 then
+      arg2 = identity[arg1]
+      if arg2 or arg1 == "imagePath" then
+        return type(arg2) == "string" and arg2 or sb.printJson(arg2, 1)
       else
-        return string.format("%s is not a part of the identity", path)
+        return string.format("%s is not a part of the identity", arg1)
       end
     end
     return sb.printJson(player.humanoidIdentity(), 1)
-  elseif method == "set" and path then
+  elseif method == "set" and arg1 then
     local identity = player.humanoidIdentity()
-    local prev = identity[path]
-    if path ~= "imagePath" then
+    local prev = identity[arg1]
+    if arg1 ~= "imagePath" then
       if not prev then
-        return string.format("%s is not a part of the identity", path)
-      elseif type(prev) ~= type(value) then
-        return string.format("%s must be a %s", path, type(prev))
+        return string.format("%s is not a part of the identity", arg1)
+      elseif type(prev) ~= type(arg2) then
+        return string.format("%s must be a %s", arg1, type(prev))
       end
-    elseif type(value) ~= "nil" and type(value) ~= "string" then
+    elseif type(arg2) ~= "nil" and type(arg2) ~= "string" then
       return "imagePath must be null or a string"
     end
-    identity[path] = value
+    identity[arg1] = arg2
     player.setHumanoidIdentity(identity)
-    return string.format("Set %s to %s", path, type(value) == "string" and value or sb.printJson(value, 1))
-  else 
-    return "Usage: /identity <get|set> [path] [value]"
+    return string.format("Set %s to %s", arg1, type(arg2) == "string" and arg2 or sb.printJson(arg2, 1))
+  elseif method == "randomize" then
+    local newIdentity = root.generateHumanoidIdentity(arg1 or player.species(), nil, player.gender())
+    player.setHumanoidIdentity(newIdentity)
+    return "Randomized identity"
+  elseif method == "save" then
+    if type(arg1) == "string" then
+      if arg1:find("%.") then
+        return "Name cannot contain '.'"
+      end
+            root.setConfigurationPath("savedHumanoidIdentities." .. arg1, player.humanoidIdentity())
+      return "Saved identity to starbound.config:savedHumanoidIdentities."..arg1
+    else
+      return "You must provide a name for this identity"
+    end
+  elseif method == "load" then
+    if type(arg1) == "string" then
+      local identity = root.getConfigurationPath("savedHumanoidIdentities." .. arg1)
+      if identity then
+        player.setHumanoidIdentity(identity)
+        return "Loaded identity from starbound.config:savedHumanoidIdentities."..arg1
+      else
+        return "No saved identity named "..arg1
+      end
+    else
+      return "You must provide a name to load from"
+    end
+  else
+    return "Usage: /identity <get|set> [path] [value]\n/identity randomize [species]\n/identity <save|load> [name]"
   end
 end)
 
