4bf5baae11b5252bd761b1cd8d37bde733d415ef
Kae <80987908+Novaenia@users.noreply.github.com>
2025-09-25 13:18:28 +1000

    make WorldServer::wire index vars not const&

--- source/game/StarWorldServer.cpp
+++ source/game/StarWorldServer.cpp
@@ -2626,7 +2626,7 @@ void WorldServer::setTemplate(WorldTemplatePtr newTemplate) {
   }
 }
 
-void WorldServer::wire(Vec2I const& outputPosition, size_t const& outputIndex, Vec2I const& inputPosition, size_t const& inputIndex) {
+void WorldServer::wire(Vec2I const& outputPosition, size_t outputIndex, Vec2I const& inputPosition, size_t inputIndex) {
   WireConnection output = {outputPosition, outputIndex};
   WireConnection input = {inputPosition, inputIndex};
   for (auto source : atTile<WireEntity>(input.entityLocation)) {
