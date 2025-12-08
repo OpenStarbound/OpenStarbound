c4cc08cbfefb945368ab918ffe486b2f62c44b12
WereTech <17415815+WereTech@users.noreply.github.com>
2025-12-07 20:36:13 -0600

    missed the override

--- source/application/StarUserGeneratedContentService_pc_steam.hpp
+++ source/application/StarUserGeneratedContentService_pc_steam.hpp
@@ -13,7 +13,7 @@ public:
   StringList subscribedContentIds() const override;
   Maybe<String> contentDownloadDirectory(String const& contentId) const override;
   bool triggerContentDownload() override;
-  bool contentNeedsDownload() const;
+  bool contentNeedsDownload() const override;
 
 private:
   STEAM_CALLBACK(SteamUserGeneratedContentService, onDownloadResult, DownloadItemResult_t, m_callbackDownloadResult);
