f1c30b07c12fed2dde008c9f7efa5cb09ace7428
grbr404 <166065505+grbr404@users.noreply.github.com>
2025-12-27 18:36:19 +0100

    Update StarRenderer_opengl.cpp

--- source/application/StarRenderer_opengl.cpp
+++ source/application/StarRenderer_opengl.cpp
@@ -165,14 +165,14 @@ OpenGlRenderer::GlFrameBuffer::GlFrameBuffer(Json const& fbConfig) : config(fbCo
   }
   auto addressing = TextureAddressingNames.getLeft(config.getString("textureAddressing", "clamp"));
   auto filtering = TextureFilteringNames.getLeft(config.getString("textureFiltering", "nearest"));
-  if (addressing == TextureAddressing::Clamp) {
-    glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  } else {
-    glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  }
   if (!multisample) {
+    if (addressing == TextureAddressing::Clamp) {
+      glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+      glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    } else {
+      glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_REPEAT);
+      glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    }
     if (filtering == TextureFiltering::Nearest) {
       glTexParameterf(target, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
       glTexParameterf(target, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
