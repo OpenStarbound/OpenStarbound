362b9593702c338c7bb873d28c15b2826e136d5d
Bottinator22 <bottinator22@gmail.com>
2026-05-22 10:18:20 -0700

    oh right it might not be updated on default. also change how configs are loaded a bit

--- assets/opensb/scripts/opensb/universeclient/loadconfig.lua
+++ assets/opensb/scripts/opensb/universeclient/loadconfig.lua
@@ -7,28 +7,26 @@ function module.init()
 	local shaderConfig = root.getConfiguration("postProcessGroups") or {}
 	local postProcessGroups = renderer.postProcessGroups()
 	local changes = false
-	for k,v in next, shaderConfig do
-		local group = postProcessGroups[k]
-		if group then
-			if group.enabledParameter then
-				-- this group might need to handle enable/disabling itself, since maybe it patches another shader that can't be disabled
-				for _,e in next, group.enabledParameterEffects do
-					renderer.setEffectParameter(e,group.enabledParameter,not not v.enabled)
-				end
+	for gname,group in next, postProcessGroups do
+		local cfg = shaderConfig[gname] or {enabled=group.enabledDefault}
+		if group.enabledParameter then
+			-- this group might need to handle enable/disabling itself, since maybe it patches another shader that can't be disabled
+			for _,e in next, group.enabledParameterEffects do
+				renderer.setEffectParameter(e,group.enabledParameter,not not cfg.enabled)
 			end
-			if v.parameters then
-				for k2,v2 in next, group.parameters do
-					local param = v.parameters[k2]
-					if param ~= nil then
-						if v2.isPasses then
-							-- this parameter controls passes
-							for _,l in next, v2.layers do
-								renderer.setPostProcessLayerPasses(l,param)
-							end
-						else
-							for _,e in next, v2.effects do
-								renderer.setEffectParameter(e,k2,param)
-							end
+		end
+		if cfg.parameters then
+			for k2,v2 in next, group.parameters do
+				local param = cfg.parameters[k2]
+				if param ~= nil then
+					if v2.isPasses then
+						-- this parameter controls passes
+						for _,l in next, v2.layers do
+							renderer.setPostProcessLayerPasses(l,param)
+						end
+					else
+						for _,e in next, v2.effects do
+							renderer.setEffectParameter(e,k2,param)
 						end
 					end
 				end
