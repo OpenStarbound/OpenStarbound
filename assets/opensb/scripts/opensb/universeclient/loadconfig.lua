9809b63d580f9a0562a8a6f291239b610301d106
Bottinator22 <bottinator22@gmail.com>
2026-05-22 10:08:18 -0700

    post process groups can also set effect parameters for their being enabled/disabled

--- assets/opensb/scripts/opensb/universeclient/loadconfig.lua
+++ assets/opensb/scripts/opensb/universeclient/loadconfig.lua
@@ -9,18 +9,26 @@ function module.init()
 	local changes = false
 	for k,v in next, shaderConfig do
 		local group = postProcessGroups[k]
-		if group and v.parameters then
-			for k2,v2 in next, group.parameters do
-				local param = v.parameters[k2]
-				if param ~= nil then
-					if v2.isPasses then
-						-- this parameter controls passes
-						for _,l in next, v2.layers do
-							renderer.setPostProcessLayerPasses(l,param)
-						end
-					else
-						for _,e in next, v2.effects do
-							renderer.setEffectParameter(e,k2,param)
+		if group then
+			if group.enabledParameter then
+				-- this group might need to handle enable/disabling itself, since maybe it patches another shader that can't be disabled
+				for _,e in next, group.enabledParameterEffects do
+					renderer.setEffectParameter(e,group.enabledParameter,not not v.enabled)
+				end
+			end
+			if v.parameters then
+				for k2,v2 in next, group.parameters do
+					local param = v.parameters[k2]
+					if param ~= nil then
+						if v2.isPasses then
+							-- this parameter controls passes
+							for _,l in next, v2.layers do
+								renderer.setPostProcessLayerPasses(l,param)
+							end
+						else
+							for _,e in next, v2.effects do
+								renderer.setEffectParameter(e,k2,param)
+							end
 						end
 					end
 				end
