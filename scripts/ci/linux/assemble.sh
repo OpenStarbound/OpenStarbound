fb197fd59467d54d60d3a9030621a688bb5f774d
grbr404 <166065505+grbr404@users.noreply.github.com>
2026-07-25 23:30:41 +0200

    add install-desktop.sh to assemble.sh

--- scripts/ci/linux/assemble.sh
+++ scripts/ci/linux/assemble.sh
@@ -21,6 +21,7 @@ cp \
   lib/linux/libsteam_api.so \
   scripts/ci/linux/sbinit.config \
   scripts/ci/linux/run-client.sh \
+  scripts/ci/linux/install-desktop.sh \
   scripts/steam_appid.txt \
   client_distribution/linux/
   
