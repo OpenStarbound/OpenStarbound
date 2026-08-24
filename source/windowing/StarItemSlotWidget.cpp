ad262a887246387efa84dd116fe147a8e265643d
SilverSokolova <80606782+SilverSokolova@users.noreply.github.com>
2026-08-24 01:45:36 -0500

    Update StarItemSlotWidget.cpp

--- source/windowing/StarItemSlotWidget.cpp
+++ source/windowing/StarItemSlotWidget.cpp
@@ -203,7 +203,7 @@ void ItemSlotWidget::renderImpl() {
     List<Drawable> iconDrawables = m_showSecondaryIcon ? m_item->secondaryDrawables().value(m_item->iconDrawables()) : m_item->iconDrawables();
 
     if (m_showRarity) {
-      String border = rarityBorder(m_item->instanceValue("rarityBorder", m_item->rarity()));
+      String border = rarityBorder(m_item->instanceValue("rarityBorder", RarityNames.getRight(m_item->rarity())).toLower());
       context()->drawInterfaceQuad(border, Vec2F(screenPosition()));
     }
 
