f7580c9434cd8e6e299e9ec63dc5d5c676668eb4
grbr404 <166065505+grbr404@users.noreply.github.com>
2026-08-29 18:09:19 +0200

    Update StarMainApplication_sdl.cpp

--- source/application/StarMainApplication_sdl.cpp
+++ source/application/StarMainApplication_sdl.cpp
@@ -579,7 +579,7 @@ public:
 #endif
 
     Logger::info("Application: Creating SDL OpenGL context");
-    static const std::pair<int,int> versions[] = {{4,6},{4,5},{4,4},{4,3},{4,2},{4,1},{4,0},{3,3},{3,2}};
+    static const std::pair<int,int> versions[] = {{4,6},{4,5},{4,4},{4,3},{4,2},{4,1},{4,0},{3,3},{3,2},{3,1}};
     for (auto& [majorVersion, minorVersion] : versions) {
       SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
       SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
